1efd69cdbc388fbd9ffd858327e907257d022c17
ImageLinx <167000450+ImageLinx@users.noreply.github.com>
2024-04-30 17:45:25 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -6,6 +6,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const uploaded = urlParams.get('uploaded');
+  const error = urlParams.get('error');
   const file = urlParams.get('file');
 
   if (uploaded && file) {
@@ -17,5 +18,15 @@ window.addEventListener('DOMContentLoaded', (event) => {
     title.textContent = 'Imagen Subida';
     description.textContent = fileLink;
     uploadForm.style.display = 'none';
+  } else if (error) {
+    const title = document.getElementById('title');
+    const description = document.getElementById('description');
+    const uploadForm = document.getElementById('upload-form');
+
+    title.textContent = 'Error al subir imagen';
+    description.textContent = decodeURIComponent(error);
+    description.style.color = 'red';
+    uploadForm.reset();
   }
 });
+
